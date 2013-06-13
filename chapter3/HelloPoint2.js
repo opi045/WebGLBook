67294973b4600571637d8f54252654c160bf2865
ji___ <siwakaka@gmail.com>
2013-06-14 02:07:15 +0900

    Updated HelloPoint2 and fixed HelloPoint1.

--- chapter3/HelloPoint2.js
+++ chapter3/HelloPoint2.js
@@ -20,9 +20,20 @@ function main()
     var fsSource = getShaderSource('shader-fs');
     if (!initShaders(gl, vsSource, fsSource))
     {
-        console.log("the shaders don't be initialized.");
+        console.log("The shaders don't be initialized.");
         return;
     }
+
+    // Get attribute variable's location
+    var attrPosition = gl.getAttribLocation(gl.program, 'attrPosition');
+    if (attrPosition < 0)
+    {
+        console.log("Can't get attribute variable's location.");
+        return;
+    }
+
+    // Assign attribute variable a point position
+    gl.vertexAttrib3f(attrPosition, 0.3, 0.3, 0.0);
 
     // Set clear color
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
